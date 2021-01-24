68253822db6b7f14e02ca602f34c80d739274cb0
Sasha Chadaev <chedosaf@mail.ru>
2021-01-24 18:37:58 +0300

    refactoring main.js

--- main.js
+++ main.js
@@ -17,19 +17,21 @@ const compare = (keys, json1, json2) => {
   const array = {};
   for (let i = 0; i < keys.length; i += 1) {
     const iterialKey = keys[i];
-    const key1 = `- ${iterialKey}`;
-    const key2 = `+ ${iterialKey}`;
-    if (json1[iterialKey] && json2[iterialKey]) {
-      if (json1[iterialKey] === json2[iterialKey]) {
-        array[`  ${iterialKey}`] = json1[iterialKey];
-      } if (json1[iterialKey] !== json2[iterialKey]) {
-        array[key1] = json1[iterialKey];
-        array[key2] = json2[iterialKey];
+    const deletedKey = `- ${iterialKey}`;
+    const addedKey = `+ ${iterialKey}`;
+    const iterialJson1Value = json1[iterialKey];
+    const iterialJson2Value = json2[iterialKey];
+    if (iterialJson1Value && iterialJson2Value) {
+      if (iterialJson1Value === iterialJson2Value) {
+        array[`  ${iterialKey}`] = iterialJson1Value;
+      } if (iterialJson1Value !== iterialJson2Value) {
+        array[deletedKey] = iterialJson1Value;
+        array[addedKey] = iterialJson2Value;
       }
-    } if (json1[iterialKey] !== undefined && json2[iterialKey] === undefined) {
-      array[`- ${iterialKey}`] = json1[iterialKey];
-    } if (json1[iterialKey] === undefined && json2[iterialKey] !== undefined) {
-      array[`+ ${iterialKey}`] = json2[iterialKey];
+    } if (iterialJson1Value !== undefined && iterialJson2Value === undefined) {
+      array[deletedKey] = json1[iterialKey];
+    } if (iterialJson1Value === undefined && iterialJson2Value !== undefined) {
+      array[addedKey] = json2[iterialKey];
     }
   } return array;
 };
@@ -41,6 +43,7 @@ const genDiff = (filepath1, filepath2) => {
   const jsonObj2 = JSON.parse(json2);
   const keys = createSharedKeys(jsonObj1, jsonObj2).sort();
   const array = compare(keys, jsonObj1, jsonObj2);
+  console.log(JSON.stringify(array, null, 2).replace(/"/g, '').replace(/,/g, ''));
   return JSON.stringify(array, null, 2).replace(/"/g, '').replace(/,/g, '');
 };
 
