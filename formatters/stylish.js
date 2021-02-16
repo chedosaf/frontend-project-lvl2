e17d6040a4d82b34ae84f5f65afa0b369f752b85
Sasha Chadaev <chedosaf@mail.ru>
2021-02-16 23:38:53 +0300

    proj step 8.5

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -8,27 +8,27 @@ const formateStylish = (mass) => {
   const stylish = (mas) => mas.reduce((previousValue, correntValue) => {
     if (correntValue.type === 'unchanged') {
       if (correntValue.children.length !== 0) {
-        str = `\n${indent.repeat(correntValue.depth)}${unchanged}${correntValue.name}: {${stylish(correntValue.children)}\n  ${indent.repeat(correntValue.depth)}}`;
+        str = `\n ${indent.repeat(correntValue.depth)}${unchanged}${correntValue.name}: {${stylish(correntValue.children)}\n   ${indent.repeat(correntValue.depth)}}`;
       } if (correntValue.children.length === 0) {
-        str = `\n${indent.repeat(correntValue.depth)}${unchanged}${correntValue.name}: ${correntValue.value}`;
+        str = `\n ${indent.repeat(correntValue.depth)}${unchanged}${correntValue.name}: ${correntValue.value}`;
       }
     } if (correntValue.type === 'deleted' || correntValue.type === 'removed') {
       if (correntValue.children.length !== 0) {
-        str = `\n${indent.repeat(correntValue.depth)}${deleted}${correntValue.name}: {${stylish(correntValue.children)}\n  ${indent.repeat(correntValue.depth)}}`;
+        str = `\n ${indent.repeat(correntValue.depth)}${deleted}${correntValue.name}: {${stylish(correntValue.children)}\n   ${indent.repeat(correntValue.depth)}}`;
       } if (correntValue.children.length === 0) {
-        str = `\n${indent.repeat(correntValue.depth)}${deleted}${correntValue.name}: ${correntValue.value}`;
+        str = `\n ${indent.repeat(correntValue.depth)}${deleted}${correntValue.name}: ${correntValue.value}`;
       }
     } if (correntValue.type === 'added' || correntValue.type === 'updated') {
       if (correntValue.children.length !== 0) {
-        str = `\n${indent.repeat(correntValue.depth)}${added}${correntValue.name}: {${stylish(correntValue.children)}\n  ${indent.repeat(correntValue.depth)}}`;
+        str = `\n ${indent.repeat(correntValue.depth)}${added}${correntValue.name}: {${stylish(correntValue.children)}\n   ${indent.repeat(correntValue.depth)}}`;
       } if (correntValue.children.length === 0) {
-        str = `\n${indent.repeat(correntValue.depth)}${added}${correntValue.name}: ${correntValue.value}`;
+        str = `\n ${indent.repeat(correntValue.depth)}${added}${correntValue.name}: ${correntValue.value}`;
       }
     }
     return previousValue + str;
   }, '');
   const a = stylish(mass);
-  return `{${a}\n}`;
+  return `{${a}\n }`;
 };
 
 export default formateStylish;
