9a4beef357df8b0c45f6029d1dba284ef7c12312
Sasha Chadaev <chedosaf@mail.ru>
2021-02-28 19:30:50 +0300

    edit becouse Cognitive Complexity

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -3,30 +3,27 @@ import _ from 'lodash';
 const complex = '[complex value]';
 const createQuotes = (item) => ((typeof item === 'boolean' || item === null || item === 0) ? item : `'${item}'`);
 
-const formatePlain = (mass) => {
-  const plain = (mas) => mas.reduce((acc, corrent) => {
-    const makeString = (item) => {
-      switch (item.type) {
-        case 'attachment': {
-          return plain(corrent.children);
-        }
-        case 'updated': {
-          return `Property '${item.path.join('')}${item.name}' was updated. From ${!_.isObject(item.prevValue) ? createQuotes(item.prevValue) : complex} to ${!_.isObject(item.value) ? createQuotes(item.value) : complex}\n`;
-        }
-        case 'removed': {
-          return `Property '${item.path.join('')}${item.name}' was removed\n`;
-        }
-        case 'added': {
-          return `Property '${item.path.join('')}${item.name}' was added with value: ${item.children.length === 0 ? createQuotes(item.value) : complex}\n`;
-        }
-        default: {
-          return '';
-        }
+const formatePlain = (mass) => mass.reduce((acc, corrent) => {
+  const makeString = (item) => {
+    switch (item.type) {
+      case 'attachment': {
+        return `${formatePlain(corrent.children)}\n`;
       }
-    };
-    return acc + makeString(corrent);
-  }, '');
-  return plain(mass).trim();
-};
+      case 'updated': {
+        return `Property '${item.path.join('')}${item.name}' was updated. From ${!_.isObject(item.prevValue) ? createQuotes(item.prevValue) : complex} to ${!_.isObject(item.value) ? createQuotes(item.value) : complex}\n`;
+      }
+      case 'removed': {
+        return `Property '${item.path.join('')}${item.name}' was removed\n`;
+      }
+      case 'added': {
+        return `Property '${item.path.join('')}${item.name}' was added with value: ${item.children.length === 0 ? createQuotes(item.value) : complex}\n`;
+      }
+      default: {
+        return '';
+      }
+    }
+  };
+  return acc + makeString(corrent);
+}, '').trim();
 
 export default formatePlain;
