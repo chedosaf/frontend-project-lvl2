d2e078c0b9ffe23b8572780d5d90c22bcec34ee2
Sasha Chadaev <chedosaf@mail.ru>
2021-02-28 21:59:32 +0300

    edit becouse Cognitive Complexity

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -5,29 +5,31 @@ const added = '+ ';
 
 const formateStylish = (mass) => {
   const stylish = (mas) => mas.reduce((previousValue, correntValue) => {
+    const makeStrWithChildren = (value) => `\n  ${indent.repeat(correntValue.depth)}${value}${correntValue.name}: {${stylish(correntValue.children)}\n    ${indent.repeat(correntValue.depth)}}`;
+    const makeStrWithoutChildren = (value) => `\n  ${indent.repeat(correntValue.depth)}${value}${correntValue.name}: ${correntValue.value}`;
     if ((correntValue.type === 'unchanged' || correntValue.type === 'attachment')
     && (correntValue.children.length !== 0)) {
-      const str = `\n  ${indent.repeat(correntValue.depth)}${unchanged}${correntValue.name}: {${stylish(correntValue.children)}\n    ${indent.repeat(correntValue.depth)}}`;
+      const str = makeStrWithChildren(unchanged);
       return previousValue + str;
     } if ((correntValue.type === 'unchanged' || correntValue.type === 'attachment')
     && (correntValue.children.length === 0)) {
-      const str = `\n  ${indent.repeat(correntValue.depth)}${unchanged}${correntValue.name}: ${correntValue.value}`;
+      const str = makeStrWithoutChildren(unchanged);
       return previousValue + str;
     } if ((correntValue.type === 'deleted' || correntValue.type === 'removed')
       && (correntValue.children.length !== 0)) {
-      const str = `\n  ${indent.repeat(correntValue.depth)}${deleted}${correntValue.name}: {${stylish(correntValue.children)}\n    ${indent.repeat(correntValue.depth)}}`;
+      const str = makeStrWithChildren(deleted);
       return previousValue + str;
     } if ((correntValue.type === 'deleted' || correntValue.type === 'removed')
     && (correntValue.children.length === 0)) {
-      const str = `\n  ${indent.repeat(correntValue.depth)}${deleted}${correntValue.name}: ${correntValue.value}`;
+      const str = makeStrWithoutChildren(deleted);
       return previousValue + str;
     } if ((correntValue.type === 'added' || correntValue.type === 'updated')
       && (correntValue.children.length !== 0)) {
-      const str = `\n  ${indent.repeat(correntValue.depth)}${added}${correntValue.name}: {${stylish(correntValue.children)}\n    ${indent.repeat(correntValue.depth)}}`;
+      const str = makeStrWithChildren(added);
       return previousValue + str;
     } if ((correntValue.type === 'added' || correntValue.type === 'updated')
       && (correntValue.children.length === 0)) {
-      const str = `\n  ${indent.repeat(correntValue.depth)}${added}${correntValue.name}: ${correntValue.value}`;
+      const str = makeStrWithoutChildren(added);
       return previousValue + str;
     } return previousValue;
   }, '');
