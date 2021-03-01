447d9def8958e8d786d908bdd31afc937a389768
Sasha Chadaev <chedosaf@mail.ru>
2021-03-01 23:40:16 +0300

    refactoring ast

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -14,25 +14,27 @@ const makeObjToStylishString = (obj, depth) => {
   return `{\n${indent.repeat(depth)}${str}}`;
 };
 
+const stylish = (mas) => mas.reduce((previousValue, correntValue) => {
+  const makeStr = (type) => `\n  ${indent.repeat(correntValue.depth)}${type}${correntValue.name}: ${_.isObject(correntValue.value) ? makeObjToStylishString(correntValue.value, correntValue.depth + 1) : correntValue.value}`;
+  if (correntValue.type === 'attachment') {
+    const str = `\n  ${indent.repeat(correntValue.depth)}${unchanged}${correntValue.name}: {${stylish(correntValue.children)}\n    ${indent.repeat(correntValue.depth)}}`;
+    return previousValue + str;
+  } if (correntValue.type === 'unchanged') {
+    const str = `\n  ${indent.repeat(correntValue.depth)}${unchanged}${correntValue.name}: ${correntValue.value}`;
+    return previousValue + str;
+  } if (correntValue.type === 'deleted') {
+    const str = makeStr(deleted);
+    return previousValue + str;
+  } if (correntValue.type === 'added') {
+    const str = makeStr(added);
+    return previousValue + str;
+  } if (correntValue.type === 'updated') {
+    const str = `\n  ${indent.repeat(correntValue.depth)}${deleted}${correntValue.name}: ${_.isObject(correntValue.prevValue) ? makeObjToStylishString(correntValue.prevValue, correntValue.depth + 1) : correntValue.prevValue}\n  ${indent.repeat(correntValue.depth)}${added}${correntValue.name}: ${_.isObject(correntValue.newValue) ? makeObjToStylishString(correntValue.newValue, correntValue.depth + 1) : correntValue.newValue}`;
+    return previousValue + str;
+  } return previousValue;
+}, '');
+
 const formateStylish = (mass) => {
-  const stylish = (mas) => mas.reduce((previousValue, correntValue) => {
-    if (correntValue.type === 'attachment') {
-      const str = `\n  ${indent.repeat(correntValue.depth)}${unchanged}${correntValue.name}: {${stylish(correntValue.children)}\n    ${indent.repeat(correntValue.depth)}}`;
-      return previousValue + str;
-    } if (correntValue.type === 'unchanged') {
-      const str = `\n  ${indent.repeat(correntValue.depth)}${unchanged}${correntValue.name}: ${correntValue.value}`;
-      return previousValue + str;
-    } if (correntValue.type === 'deleted') {
-      const str = `\n  ${indent.repeat(correntValue.depth)}${deleted}${correntValue.name}: ${_.isObject(correntValue.value) ? makeObjToStylishString(correntValue.value, correntValue.depth + 1) : correntValue.value}`;
-      return previousValue + str;
-    } if (correntValue.type === 'added') {
-      const str = `\n  ${indent.repeat(correntValue.depth)}${added}${correntValue.name}: ${_.isObject(correntValue.value) ? makeObjToStylishString(correntValue.value, correntValue.depth + 1) : correntValue.value}`;
-      return previousValue + str;
-    } if (correntValue.type === 'updated') {
-      const str = `\n  ${indent.repeat(correntValue.depth)}${deleted}${correntValue.name}: ${_.isObject(correntValue.prevValue) ? makeObjToStylishString(correntValue.prevValue, correntValue.depth + 1) : correntValue.prevValue}\n  ${indent.repeat(correntValue.depth)}${added}${correntValue.name}: ${_.isObject(correntValue.newValue) ? makeObjToStylishString(correntValue.newValue, correntValue.depth + 1) : correntValue.newValue}`;
-      return previousValue + str;
-    } return previousValue;
-  }, '');
   const a = stylish(mass);
   return `{${a}\n}`;
 };
