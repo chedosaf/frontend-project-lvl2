4be099d27b824063998a9e22a30df9b9906de329
Sasha Chadaev <chedosaf@mail.ru>
2021-03-19 22:06:48 +0300

    edit becouse of mentor comments pt.4.3

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -8,28 +8,28 @@ const createSpace = (depth) => indent.repeat(depth);
 
 const makeStylishString = (obj, depth) => {
   const keys = _.sortBy(Object.keys(obj));
-  const createStr = keys.reduce((acc, current) => {
-    const value = obj[current];
-    const str = `    ${current}: ${_.isObject(value) ? makeStylishString(value, depth + 1) : value}\n${createSpace(depth)}`;
+  const createStr = keys.reduce((acc, cur) => {
+    const value = obj[cur];
+    const str = `    ${cur}: ${_.isObject(value) ? makeStylishString(value, depth + 1) : value}\n${createSpace(depth)}`;
     return acc + str;
   }, '');
   return `{\n${createSpace(depth)}${createStr}}`;
 };
 
-const stylish = (arr, depth = 0) => arr.reduce((acc, currentValue) => {
-  const makeStr = (type) => `${acc}\n  ${createSpace(depth)}${type}${currentValue.key}: ${_.isObject(currentValue.value) ? makeStylishString(currentValue.value, depth + 1) : currentValue.value}`;
-  switch (currentValue.type) {
+const stylish = (arr, depth = 0) => arr.reduce((acc, cur) => {
+  const makeStr = (type) => `${acc}\n  ${createSpace(depth)}${type}${cur.key}: ${_.isObject(cur.value) ? makeStylishString(cur.value, depth + 1) : cur.value}`;
+  switch (cur.type) {
     case 'attachment':
-      return `${acc}\n  ${createSpace(depth)}  ${currentValue.key}: {${stylish(currentValue.children, depth + 1)}\n    ${createSpace(depth)}}`;
+      return `${acc}\n  ${createSpace(depth)}  ${cur.key}: {${stylish(cur.children, depth + 1)}\n    ${createSpace(depth)}}`;
     case 'unchanged':
-      return `${acc}\n  ${createSpace(depth)}  ${currentValue.key}: ${currentValue.value}`;
+      return `${acc}\n  ${createSpace(depth)}  ${cur.key}: ${cur.value}`;
     case 'deleted':
       return makeStr(deleted);
     case 'added':
       return makeStr(added);
     case 'updated':
-      return `${acc}\n  ${createSpace(depth)}- ${currentValue.key}: ${_.isObject(currentValue.prevValue) ? makeStylishString(currentValue.prevValue, depth + 1) : currentValue.prevValue}
-  ${createSpace(depth)}+ ${currentValue.key}: ${_.isObject(currentValue.value) ? makeStylishString(currentValue.value, depth + 1) : currentValue.value}`;
+      return `${acc}\n  ${createSpace(depth)}- ${cur.key}: ${_.isObject(cur.prevValue) ? makeStylishString(cur.prevValue, depth + 1) : cur.prevValue}
+  ${createSpace(depth)}+ ${cur.key}: ${_.isObject(cur.value) ? makeStylishString(cur.value, depth + 1) : cur.value}`;
     default:
       return acc;
   }
