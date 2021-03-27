8d87ac1bafdedad4df7da0d9ba31f29a41fe7089
Sasha Chadaev <chedosaf@mail.ru>
2021-03-27 17:10:20 +0300

    edit becouse of mentor comments pt.5.2

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -18,13 +18,18 @@ const makeStylishString = (obj, depth) => {
 
 const stylish = (arr, depth = 1) => {
   const stringify = (obj, type = indent) => {
-    if (!_.has(obj, 'value')) {
-      return `${createSpace(depth)}${type}${obj.key}: ${stylish(obj.children, depth + 2)}`;
-    } if (_.has(obj, 'prevValue')) {
-      return `${createSpace(depth)}- ${obj.key}: ${_.isObject(obj.prevValue)
-        ? makeStylishString(obj.prevValue, depth + 2) : obj.prevValue}\n${createSpace(depth)}+ ${obj.key}: ${_.isObject(obj.value)
-        ? makeStylishString(obj.value, depth + 2) : obj.value}`;
-    } return `${createSpace(depth)}${type}${obj.key}: ${_.isObject(obj.value) ? makeStylishString(obj.value, depth + 2) : obj.value}`;
+    switch (true) {
+      case (!_.has(obj, 'value')):
+        return `${createSpace(depth)}${type}${obj.key}: ${stylish(obj.children, depth + 2)}`;
+      case (_.has(obj, 'prevValue')):
+        return `${createSpace(depth)}- ${obj.key}: ${_.isObject(obj.prevValue)
+          ? makeStylishString(obj.prevValue, depth + 2) : obj.prevValue}\n${createSpace(depth)}+ ${obj.key}: ${_.isObject(obj.value)
+          ? makeStylishString(obj.value, depth + 2) : obj.value}`;
+      case (_.has(obj, 'value')):
+        return `${createSpace(depth)}${type}${obj.key}: ${_.isObject(obj.value) ? makeStylishString(obj.value, depth + 2) : obj.value}`;
+      default:
+        throw Error('Wrong type of node!');
+    }
   };
   const arrey = arr.flatMap((cur) => {
     switch (cur.type) {
