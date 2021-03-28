cb08ab9027194ca32c53a26f03ac4838ecc9cd92
Sasha Chadaev <chedosaf@mail.ru>
2021-03-28 16:21:53 +0300

    edit becouse of mentor comments pt.5.2

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -6,15 +6,20 @@ const getPath = (path, cur) => [...path, cur].join('.');
 
 const stringify = (node, path, fn) => {
   const strValue = (value) => (!_.isObject(value) ? createQuotes(value) : complex);
-  if (node.type === 'updated') {
-    return `Property '${getPath(path, node.key)}' was updated. From ${strValue(node.prevValue)} to ${strValue(node.value)}`;
-  } if (node.type === 'deleted') {
-    return `Property '${getPath(path, node.key)}' was removed`;
-  } if (node.type === 'added') {
-    return `Property '${getPath(path, node.key)}' was added with value: ${!_.isObject(node.value) ? createQuotes(node.value) : complex}`;
-  } if (node.type === 'attachment') {
-    return `${fn(node.children, [...path, node.key])}`;
-  } return [];
+  switch (true) {
+    case (node.type === 'updated'):
+      return `Property '${getPath(path, node.key)}' was updated. From ${strValue(node.prevValue)} to ${strValue(node.value)}`;
+    case (node.type === 'deleted'):
+      return `Property '${getPath(path, node.key)}' was removed`;
+    case (node.type === 'added'):
+      return `Property '${getPath(path, node.key)}' was added with value: ${!_.isObject(node.value) ? createQuotes(node.value) : complex}`;
+    case (node.type === 'attachment'):
+      return `${fn(node.children, [...path, node.key])}`;
+    case (node.type === 'unchanged'):
+      return [];
+    default:
+      throw Error('Wrong node type');
+  }
 };
 
 const formatePlain = (arr, path = []) => arr.flatMap((current) => stringify(current, path, formatePlain)).join('\n');
